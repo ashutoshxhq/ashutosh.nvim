f458b7bf3960a9d675c7514f0c003967b0a55fdd
Ashutosh Dubey <ashutosh.dubey@ibexlabs.com>
2024-02-08 16:25:24 +0530

    initial commit

---
+++ lua/ashutosh/init.lua
@@ -0,0 +1,6 @@
+require("ashutosh.lazy")
+require("ashutosh.keymaps")
+require("ashutosh.config")
+require("ashutosh.lspconfig")
+require("ashutosh.set")
+
---
+++ lua/ashutosh/keymaps.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+-- Telescope Keymaps
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git); 
+
